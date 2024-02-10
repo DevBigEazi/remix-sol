c36616489cac79123ffab6393a8459eac4b17194
devbigeazi <adesholatajudeen1@gmail.com>
2024-02-10 15:50:38 +0100

    storage factory cont.

--- storage-factory/StorageFactory.sol
+++ storage-factory/StorageFactory.sol
@@ -4,10 +4,20 @@ pragma solidity ^0.8.0;
 import "./SimpleStorage.sol";
 
 contract StorageFactory {
-     SimpleStorage public simpleStorage;
+    //type          //visibilty     // variable name
+     SimpleStorage[] public simpleStorageArray; // this will create a view of our simpleStorage contract
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
     }
 
+    // trying to interact with store function from simpleStorage contract
+    function sfStore(uint256 _simpleStorageIndex,  uint256 _simpleStorageNum) public {
+      
+    }
+ 
 }
+
+// In other to interact with any contract, you always going to need 2 things; Address and ABI --Application Binary Interface
+// ABI will tell our code exactly how to interact with the contract
