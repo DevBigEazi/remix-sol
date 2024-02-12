1f08dc06b9b50f9e685679739de3de366041cb28
devbigeazi <adesholatajudeen1@gmail.com>
2024-02-12 11:34:37 +0100

    ...

--- storage-factory/StorageFactory.sol
+++ storage-factory/StorageFactory.sol
@@ -5,7 +5,7 @@ import "./SimpleStorage.sol";
 
 contract StorageFactory {
     //type          //visibilty     // variable name
-     SimpleStorage[] public simpleStorageArray; // this will create a view of our simpleStorage contract
+    SimpleStorage[] public simpleStorageArray; // this will create a view of our simpleStorage contract
 
     function createSimpleStorageContract() public {
         SimpleStorage simpleStorage = new SimpleStorage();
@@ -13,10 +13,17 @@ contract StorageFactory {
     }
 
     // trying to interact with store function from simpleStorage contract
-    function sfStore(uint256 _simpleStorageIndex,  uint256 _simpleStorageNum) public {
-      
+    function sfStore(
+        uint256 _simpleStorageIndex,
+        uint256 _simpleStorageNum
+    ) public {
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNum);
     }
- 
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
 }
 
 // In other to interact with any contract, you always going to need 2 things; Address and ABI --Application Binary Interface
