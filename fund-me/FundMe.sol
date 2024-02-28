ebcb182774640f683af2f10498cb25817929e741
devbigeazi <adesholatajudeen1@gmail.com>
2024-02-28 10:46:30 +0100

    assigned value to num when the fund function is called

--- fund-me/FundMe.sol
+++ fund-me/FundMe.sol
@@ -6,23 +6,20 @@
 pragma solidity ^0.8.0;
 
 contract FundMe {
-    uint256 public num;
+    uint256 public minimumUsd = 50;
 
     function fund() public payable {
         // Want to be able to set a minimum fund amount in USD
         // 1. How do we send ETH to this contract
-
-        //msg.value; // to get how much value somebody is sending
-        // if we want them to send at least 1 ETH
-        num = 5;
-        require(msg.value > 1e18, "Didn't send enough"); // 1e18 == 1* 10 ** 18 == 1000000000000000000
+        require(msg.value > minimumUsd, "Didn't send enough"); // 1e18 == 1* 10 ** 18 == 1000000000000000000
 
         // what is reverting? undo any action before, and send remaining gas back
-        
     }
 
-//     function withdraw() internal {}
+    //     function withdraw() internal {}
 }
 
 // Just like wallet hold funds, smart contratc can also hold funds
 // Money math is done in terms of wei, so ETH needs to be set as 1e18 value
+
+// Chainlink and Oracle: They come into play when we need to convert ETH to USD.
